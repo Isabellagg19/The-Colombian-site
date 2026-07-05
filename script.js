ed74d1663381bbee1649720645c4136627976349
Isabellagg19 <isabellaguioc123@gmail.com>
2026-07-05 00:47:24 -0500

    new sections and backgrounds

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const colors = [
-  '#0b0000'
+  '#000000',
+  '#58e19ad0',
 ]
 
+    window.addEventListener("scroll", () => {
+      const scrollY = window.scrollY;
+      const sectionHeight = window.innerHeight;
+      const index = Math.floor(scrollY / sectionHeight);
+
+      if (colors [index]) {
+        document.body.style.backgroundColor = colors[index];
+      }
+    });
