05127981203fe2477e9d0588390140a61e5ef014
Isabellagg19 <isabellaguioc123@gmail.com>
2026-07-17 03:27:56 -0500

    new arrow designs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const navBar = document.querySelector('.nav-bar');
-const leftArrow = document.querySelector('.arrow.left');
-const rightArrow = document.querySelector('.arrow.right');
+const leftArrow = document.querySelector('.arrow-left');
+const rightArrow = document.querySelector('.arrow-right');
 
 rightArrow.addEventListener('click', () => {
   navBar.scrollBy({ left: 1000, behavior: 'smooth' });
